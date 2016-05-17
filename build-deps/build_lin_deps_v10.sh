6d4af68509ca1a33de697a5dfd726c34bc03992d
AN <an-noreply@grassfish.com>
2016-05-17 14:39:23 +0200

    build-deps: Verschiebe Build-Deps Scripts von gfLinux in gfLinuxDeps Repo

---
+++ build-deps/build_lin_deps_v10.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+set -e
+
+sudo apt-get install -y libxcb1-dev \
+                      libxcb-shm0-dev \
+                      libxcb-composite0-dev \
+                      libxcb-xv0-dev \
+                      libasound2-dev \
+                      libva-dev \
+                      libvdpau-dev \
+                      libxv-dev \
+                      libpulse-dev \
+                      libxcb-randr0-dev \
+                      libxi-dev \
+                      mesa-common-dev \
+                      build-essential \
+                      libgl1-mesa-dev \
+                      libegl1-mesa-dev \
+                      yasm \
+                      cmake \
+                      libpangocairo-1.0-0 \
+                      libcairo2 \
+                      libpango1.0-0 \
+                      libgconf2-4 \
+                      libatk1.0-0 \
+                      libxcursor1 \
+                      chrpath \
+                      zip
+
+
+INSTALL_PREFIX=/opt/gf-builddeps
+
+export PKG_CONFIG_PATH=$INSTALL_PREFIX/lib/pkgconfig
+
+if [ ! -d deps-buildspace ]; then
+    mkdir deps-buildspace
+fi
+
+cd deps-buildspace
+
+if [ ! -e ffmpeg-2.8.6.tar.bz2 ]; then
+    wget http://ffmpeg.org/releases/ffmpeg-2.8.6.tar.bz2
+fi
+
+if [ ! -e vlc-2.2.2.tar.xz ]; then
+    wget http://get.videolan.org/vlc/2.2.2/vlc-2.2.2.tar.xz
+fi
+
+tar xvf ffmpeg-2.8.6.tar.bz2
+tar xvf vlc-2.2.2.tar.xz
+
+# BUILD FFMPEG
+cd ffmpeg-2.8.6
+./configure --extra-version=Grassfish \
+            --enable-pic \
+            --enable-runtime-cpudetect \
+            --enable-hwaccels --enable-shared --disable-static \
+            --disable-postproc \
+            --extra-cflags=' -O3 ' \
+            --extra-libs=' -lrt' \
+            --prefix=$INSTALL_PREFIX
+
+make -j`nproc`
+
+if [ -e $INSTALL_PREFIX ]; then
+    rm -rf $INSTALL_PREFIX
+fi
+
+sudo make install
+
+# GO BACK
+cd ..
+
+# BUILD VLC
+cd vlc-2.2.2
+
+./configure --prefix=$INSTALL_PREFIX \
+            --enable-x11 --enable-xvideo --disable-gtk \
+            --disable-livedotcom --disable-skins --disable-skins2 --enable-alsa --disable-kde \
+            --disable-qt --disable-wxwindows --disable-ncurses \
+            --enable-release --disable-lua --disable-mad --disable-a52 \
+            --disable-libgcrypt --disable-update-check --disable-x265 \
+            --disable-taglib --disable-gnutls
+
+make -j`nproc`
+
+sudo make install
