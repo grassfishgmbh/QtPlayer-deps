f30ed5f6d5205beffe7ddd28bc80d7788a3dc3c1
AN <an-noreply@grassfish.com>
2016-06-23 12:29:59 +0200

    QtAV: cmd Script zum Bootstrappen des ffmpeg-Bauens

---
+++ qtav/build-windows.bat
@@ -0,0 +1,9 @@
+rem bootstrap amd64 build
+set PATH=C:\cygwin64\bin;%PATH%
+call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64
+
+rem build ffmpeg
+bash ffmpeg_msvc.sh x86_64
+
+rem build QtAV
+rem bash build-qtav.sh x86_64
