3459f02f040fd7d1d5db9151da0ba517b8b9fb73
AN <an-noreply@grassfish.com>
2016-06-23 14:24:59 +0200

    ffmpeg_msvc.sh: nicht auf librt linken und INSTALL_PREFIX setzen

--- qtav/ffmpeg_msvc.sh
+++ qtav/ffmpeg_msvc.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# additionally export PATH variables as suggested by the ffmpeg wiki 
 if [ "$1" == "x86_64" ]; then
     export PATH="/c/Program Files (x86)/Microsoft Visual Studio 12.0/VC/BIN/amd64/":$PATH
 elif [ "$1" == "i686" ]; then
     export PATH="/c/Program Files (x86)/Microsoft Visual Studio 12.0/VC/BIN/":$PATH
+else
+    echo "No architecture set as argument, bailing."
+    exit 1
 fi
 
 if [ ! -d deps-buildspace ]; then
@@ -13,6 +17,8 @@ fi
 cd deps-buildspace
 
 DEPS_BS_ROOT=`pwd`
+
+INSTALL_PREFIX="$DEPS_BS_ROOT"/qtav-$1
 
 if [ ! -e ffmpeg-2.8.6.tar.bz2 ]; then
     wget http://ffmpeg.org/releases/ffmpeg-2.8.6.tar.bz2
@@ -28,7 +34,6 @@ cd ffmpeg-2.8.6
             --enable-runtime-cpudetect \
             --enable-hwaccels --enable-shared --disable-static \
             --disable-postproc \
-            --extra-libs=' -lrt' \
             --prefix=$INSTALL_PREFIX
 
 make -j`nproc`
