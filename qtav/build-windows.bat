c7c8b7af606d8851947ed9af85b754dac9ad8dee
AN <an-noreply@grassfish.com>
2016-06-23 15:42:23 +0200

    qtav/build-windows: 32bit Builds und QtAV Build Script Aufruf fixen

--- qtav/build-windows.bat
+++ qtav/build-windows.bat
@@ -1,9 +1,19 @@
-rem bootstrap amd64 build
-set PATH=C:\cygwin64\bin;%PATH%
+rem bootstrap 64 bit build first
+set PATH=C:\cygwin64\bin;C:\Qt\5.6\msvc2013_64\bin;%PATH%
 call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64
 
 rem build ffmpeg
 bash ffmpeg_msvc.sh x86_64
 
 rem build QtAV
-bash build-qtav.sh x86_64
+bash qtav_msvc.sh x86_64
+
+rem bootstrap 32 bit build now
+set PATH=C:\Qt\5.6\msvc2013\bin;%PATH%
+call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64_x86
+
+rem build ffmpeg
+bash ffmpeg_msvc.sh i686
+
+rem build QtAV
+bash qtav_msvc.sh i686
