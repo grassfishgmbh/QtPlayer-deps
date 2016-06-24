66eec99df8354ef5598204c960fa1d0d5411a1f3
AN <an-noreply@grassfish.com>
2016-06-24 14:01:33 +0200

    qtav/qtav_installation.sh: Script zum Installieren von QtAV ins gewünschte Target-Verzeichnis

---
+++ qtav/qtav_installation.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+QT_VERSION="5.6"
+
+if [ "$1" == "x86_64" ]; then
+    OLD_INSTALL_TARGET="C:\Qt\$QT_VERSION\msvc2013_64"
+elif [ "$1" == "i686" ]; then
+    OLD_INSTALL_TARGET="C:\Qt\$QT_VERSION\msvc2013"
+else
+    echo "No architecture provided as argument, bailing."
+    exit 1
+fi
+
+NEW_INSTALL_TARGET=`cygpath -w -a $PWD`\\deps-buildspace\\qtav-$1
+
+cd deps-buildspace\build-QtAV
+
+# fix the QtAV-generated install batch script to install into
+# the dedicated install target (which we zip up post build)
+sed -i 's/$OLD_INSTALL_TARGET/$NEW_INSTALL_TARGET/g' sdk_install.bat
+
+# create additional directories usually found in a Qt target
+mkdir -p $NEW_INSTALL_TARGET/mkspecs/features
+mkdir -p $NEW_INSTALL_TARGET/mkspecs/modules
+
+# install this mofucker
+cmd /C sdk_install.bat
