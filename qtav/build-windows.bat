7ab79d297f8308dd9ecdfd75598e3a0b94320371
AN <an-noreply@grassfish.com>
2016-06-24 11:31:42 +0200

    qtav/qtav_msvc: Bauen im Batch Script anstelle des Shell Scripts

--- qtav/build-windows.bat
+++ qtav/build-windows.bat
@@ -2,11 +2,28 @@ rem bootstrap 64 bit build first
 set PATH=C:\cygwin64\bin;C:\Qt\5.6\msvc2013_64\bin;%PATH%
 call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64
 
+set ARCHLESS_INCLUDE=%INCLUDE%
+set ARCHLESS_LIB=%LIB%
+
 rem build ffmpeg
-bash ffmpeg_msvc.sh x86_64
+rem bash ffmpeg_msvc.sh x86_64
+
+rem fetch QtAV
+bash qtav_msvc.sh x86_64
 
 rem build QtAV
-bash qtav_msvc.sh x86_64
+cd deps-buildspace\build-QtAV
+
+set INCLUDE=%cd%\..\qtav-x86_64\include;%INCLUDE%
+set LIB=%cd%\..\qtav-x86_64\lib;%cd%\..\qtav-x86_64\bin;%LIB%
+
+qmake ../QtAV/QtAV.pro
+jom -j8
+
+set INCLUDE=%ARCHLESS_INCLUDE%
+set LIB=%ARCHLESS_LIB%
+
+cd ../..
 
 rem bootstrap 32 bit build now
 set PATH=C:\Qt\5.6\msvc2013\bin;%PATH%
@@ -15,5 +32,19 @@ call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd6
 rem build ffmpeg
 bash ffmpeg_msvc.sh i686
 
-rem build QtAV
+rem fetch QtAV
 bash qtav_msvc.sh i686
+
+rem build QtAV
+cd deps-buildspace\build-QtAV
+
+set INCLUDE=%cd%\..\qtav-i686\include;%INCLUDE%
+set LIB=%cd%\..\qtav-i686\lib;%cd%\..\qtav-i686\bin;%LIB%
+
+qmake ../QtAV/QtAV.pro
+jom -j8
+
+set INCLUDE=%ARCHLESS_INCLUDE%
+set LIB=%ARCHLESS_LIB%
+
+cd ../..
--- qtav/qtav_msvc.sh
+++ qtav/qtav_msvc.sh
@@ -2,7 +2,7 @@
 
 # additionally export PATH variables as suggested by the ffmpeg wiki 
 if [ "$1" == "x86_64" ]; then
-    export PATH="/cygdrive/c/Program Files (x86)/Microsoft Visual Studio 12.0/VC/BIN/amd64/":$PATH
+    export PATH="/cygdrive/c/Program Files (x86)/Microsoft Visual Studio 12.0/VC/BIN/amd64:$PATH"
 elif [ "$1" == "i686" ]; then
     export PATH="/cygdrive/c/Program Files (x86)/Microsoft Visual Studio 12.0/VC/BIN/":$PATH
 else
@@ -18,13 +18,13 @@ cd deps-buildspace
 
 DEPS_BS_ROOT=`pwd`
 
-INSTALL_PREFIX="$DEPS_BS_ROOT"/qtav-$1
+INSTALL_PREFIX="$DEPS_BS_ROOT/qtav-$1"
 INSTALL_PREFIX_WIN=`cygpath $INSTALL_PREFIX -a -w`
 
 # We require ffmpeg to already exist in the INSTALL_PREFIX
 # Now set the environment variables to allow linking to ffmpeg
-export INCLUDE="$INSTALL_PREFIX_WIN"\\include:$INCLUDE
-export LIB="$INSTALL_PREFIX_WIN"\\lib:$LIB
+export INCLUDE="$INSTALL_PREFIX_WIN\\include;$INCLUDE"
+export LIB="$INSTALL_PREFIX_WIN\\lib;$INSTALL_PREFIX_WIN\\bin;$LIB"
 
 if [ -d QtAV ]; then
     rm -rf QtAV
@@ -40,6 +40,6 @@ git checkout tags/v1.11.0
 
 cd ..
 mkdir build-QtAV
-cd build-QtAV
-qmake ../QtAV/QtAV.pro PREFIX="$INSTALL_PREFIX_WIN"
-make -j`nproc`
+#cd build-QtAV
+#qmake ../QtAV/QtAV.pro PREFIX="$INSTALL_PREFIX_WIN"
+#make -j`nproc`
