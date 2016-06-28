a8cf9f8acdae024662834ec2a3d54e73170a45f1
AN <an-noreply@grassfish.com>
2016-06-28 14:00:14 +0200

    config.sh: Neue Variablen "CONFIG_DIR" und "QTAV_PATCH_DIR"

--- config.sh
+++ config.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+CONFIG_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 QT_VERSION="5.6.1-1"
 QT_DIR="/opt/Qt/$QT_VERSION/gcc_64"
 
@@ -7,6 +9,7 @@ VLC_VERSION="2.2.1"
 
 QTAV_DEVRELEASE=1
 QTAV_TAG="b842b648c9f8eda6a96ea719940aac57bed24c7f"
+QTAV_PATCH_DIR="$CONFIG_DIR/qtav/patches"
 
 FFMPEG_VERSION="2.8.6"
 
