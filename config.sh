f734eae2ec4ad062b5af780536ed969112c8b0fa
AN <an-noreply@grassfish.com>
2016-06-29 14:26:50 +0200

    config.sh: QTWEBENGINE_VERSION

--- config.sh
+++ config.sh
@@ -3,6 +3,7 @@
 CONFIG_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 QT_VERSION="5.6.1-1"
+QTWEBENGINE_VERSION="5.7.0"
 QT_DIR="/opt/Qt/$QT_VERSION/gcc_64"
 QT_PATCH_DIR="$CONFIG_DIR/qt/patches"
 
