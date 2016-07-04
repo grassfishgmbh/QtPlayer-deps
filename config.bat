bd5b8f8f493cf73faf6da8904dbed35df7971a81
AN <an-noreply@grassfish.com>
2016-07-04 08:41:50 +0200

    config.bat: Build Config im Batch Format

---
+++ config.bat
@@ -0,0 +1,15 @@
+set CONFIG_DIR=%CD%
+
+set QT_VERSION=5.6.1-1
+set QTWEBENGINE_VERSION=5.7.0
+if "%GF_QT_IS_32_BIT_BUILD%"=="" (set QT_DIR=C:\Qt\%QT_VERSION%\msvc2013_64) else (set QT_DIR=C:\Qt\%QT_VERSION%\msvc2013)
+
+set QT_PATCH_DIR=%CONFIG_DIR%\qt\patches
+
+set VLC_VERSION=2.2.1
+
+set QTAV_DEVRELEASE=1
+set QTAV_TAG=2d0dd1d1c806dba4e6ec2af58e6186df6b533c58
+set QTAV_PATCH_DIR=%CONFIG_DIR%\qtav\patches
+
+set FFMPEG_VERSION=2.8.6
