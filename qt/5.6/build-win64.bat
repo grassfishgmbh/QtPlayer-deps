e1d28173bd0736dcdaab5687948a740e20c36fe0
AN <an-noreply@grassfish.com>
2016-07-04 10:45:04 +0200

    qt/build-win64.bat: Setze BUILD_NUMBER falls nicht definiert und lese Qt Version aus Config

--- qt/5.6/build-win64.bat
+++ qt/5.6/build-win64.bat
@@ -1,5 +1,13 @@
 set PATH=C:\cygwin64\bin;%PATH%
 
+call ..\..\config.bat
+
+if not defined BUILD_NUMBER (
+set BUILD_NUMBER=1
+)
+
+set BUILD_VERSION=%QT_VERSION%-%BUILD_NUMBER%
+
 if "%GF_QT_IS_32_BIT_BUILD%"=="" (CALL "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64) else (CALL "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64_x86)
 
 bash fetch.sh
