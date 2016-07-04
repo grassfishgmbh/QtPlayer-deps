15ca7c6b39b033bbe124bee98dd1f874d2af55ef
AN <an-noreply@grassfish.com>
2016-07-04 11:41:40 +0200

    qt/5.6/build-win64: build.bat normal ausführen

--- qt/5.6/build-win64.bat
+++ qt/5.6/build-win64.bat
@@ -11,4 +11,4 @@ set BUILD_VERSION=%QT_VERSION%-%BUILD_NUMBER%
 if "%GF_QT_IS_32_BIT_BUILD%"=="" (CALL "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64) else (CALL "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64_x86)
 
 bash fetch.sh
-call build.bat
+build.bat
