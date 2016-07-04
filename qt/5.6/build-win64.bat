3b280b8ab2b3e1cb25bbb09da6c51842e813917a
AN <an-noreply@grassfish.com>
2016-07-04 08:51:58 +0200

    qt: Shim Buildscript für Windows

---
+++ qt/5.6/build-win64.bat
@@ -0,0 +1,7 @@
+set PATH="C:\cygwin64\bin";%PATH%
+
+if "%GF_QT_IS_32_BIT_BUILD%"=="" (CALL "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64) 
+else (CALL "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64_x86)
+
+bash fetch.sh
+call build.bat
