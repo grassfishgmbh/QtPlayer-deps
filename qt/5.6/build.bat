512d179f5bf3a082f95455ce171ed8ae7161733b
AN <an-noreply@grassfish.com>
2016-07-04 08:42:17 +0200

    qt/build.bat: Ungetestetes Qt Build Script für Windows

---
+++ qt/5.6/build.bat
@@ -0,0 +1,30 @@
+set BASEDIR=%CD%
+
+CALL %BASEDIR%\..\..\config.bat
+
+cd qt-src
+
+rem build qt
+configure -prefix %QT_DIR% -opensource -nomake examples -nomake tests -confirm-license -qt-zlib -qt-libpng -qt-libjpeg -opengl dynamic
+jom
+
+if exist %QT_DIR% rd /q /s %QT_DIR%
+
+jom install
+
+rem build patched WebEngine
+set PATH=%QT_DIR%\bin;%PATH%
+
+mkdir build-qtwebengine-%QTWEBENGINE_VERSION%
+cd build-qtwebengine-%QTWEBENGINE_VERSION%
+qmake ../qtwebengine-%QTWEBENGINE_VERSION%/qtwebengine.pro
+jom
+jom install
+cd ..
+
+rem zip archive for dev usage
+zip -y --symlinks -r ../Qt-%QT_VERSION%.zip %QT_DIR%
+cd ..
+
+echo %BUILD_VERSION% > version.txt
+md5sum Qt-%QT_VERSION%.zip > qtframework.txt
