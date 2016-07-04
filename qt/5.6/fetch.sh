eb2dd4a36245c04aaa741484a8e16dd8db141a12
AN <an-noreply@grassfish.com>
2016-07-04 08:40:15 +0200

    qt: Teile Qt Build Script auf

---
+++ qt/5.6/fetch.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+set -e
+
+BASEDIR=$(dirname "$0")
+
+source $BASEDIR/../../config.sh
+
+if [[ -z "$QT_VERSION" ]]; then
+	echo "QT_VERSION not set";
+    exit -1
+fi
+
+if [[ -z "$BUILD_VERSION" ]]; then
+	echo "BUILD_VERSION string not set";
+    exit -1
+fi
+
+
+rm -f *.zip
+rm -rf qt-src
+rm -f *.txt
+
+bash prepare.sh
+
+# get Qt5 main repo
+git clone http://code.qt.io/cgit/qt/qt5.git --branch v$QT_VERSION qt-src
+
+# init subrepos
+cd qt-src
+#perl init-repository --module-subset=default,-qtwebkit-examples,-qt3d,-qtactiveqt,-qtandroidextras,-qtcanvas3d,-qtpurchasing,-qtenginio,-qtmacextras,-qtpim,-qtfeedback,qtwebkit
+git submodule init qtbase qtconnectivity qtdeclarative qtdoc qtgraphicaleffects qtimageformats qtlocation qtmultimedia qtqa 
+git submodule init qtquickcontrols qtquickcontrols2 qtrepotools qtscript qtsensors  qtserialbus qtserialport qtsvg qttools
+git submodule init qttranslations qtwayland qtwebchannel qtwebkit qtwebsockets qtwebview qtwinextras qtx11extras qtxmlpatterns
+git submodule update
+
+# init subsubrepos
+cd qtxmlpatterns; git submodule update --init; cd ..
+cd qtdeclarative; git submodule update --init; cd ..
+
+# Enable GStreamer 1.0 support
+cd qtmultimedia
+grep GST_VERSION .qmake.conf || echo "GST_VERSION=1.0">>.qmake.conf
+cd ..
+
+# apply patch: protection against accessing destroyed QDBusConnectionManager
+cd qtbase
+patch -f -Np1 -i "$QT_PATCH_DIR/0006-dbus-connectionmanager-destroy.patch"
+cd ..
+
+# independently pull QtWebEngine based on the version defined in config.sh
+if [ -e qtwebengine-$QTWEBENGINE_VERSION ]; then
+    rm -rf qtwebengine-$QTWEBENGINE_VERSION
+fi
+
+git clone https://github.com/qt/qtwebengine.git qtwebengine-$QTWEBENGINE_VERSION
+cd qtwebengine-$QTWEBENGINE_VERSION
+git checkout tags/v$QTWEBENGINE_VERSION
+git submodule update --init --recursive
+
+# apply proxy patch
+patch -f -Np1 -i "$QT_PATCH_DIR/0002-disable-proxy-for-localhost.patch"
+
+# apply webchannel transport patch
+patch -f -Np1 -i "$QT_PATCH_DIR/0005-qtwebengine-5.7-reload-channel.patch"
+cd ..
+
+
+# apply in-process-gpu & vaapi patch
+cd qtwebengine-$QTWEBENGINE_VERSION/src/3rdparty/
+patch -f -Np1 -i "$QT_PATCH_DIR/0001-qtwebengine-hwaccel.patch"
+
+#enable proprietary codecs in webengine
+cd ../../
+grep use_proprietary_codecs .qmake.conf || echo "WEBENGINE_CONFIG+=use_proprietary_codecs">>.qmake.conf
+cd ..
+
---
+++ qt/5.6/prepare.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#build essential
+sudo apt-get install -y build-essential perl python git 
+sudo apt-get install -y libfontconfig1-dev libfreetype6-dev libx11-dev libxext-dev libxfixes-dev libxi-dev \
+						libxrender-dev libxcb1-dev libx11-xcb-dev libxcb-glx0-dev
+sudo apt-get install -y "^libxcb.*" libx11-xcb-dev libglu1-mesa-dev libxrender-dev libxi-dev
+sudo apt-get install -y libgstreamer1.0-dev libgstreamer-vaapi1.0-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev \
+						libgstreamer-plugins-good1.0-dev libgstreamer-plugins-bad1.0-dev 
+sudo apt-get install -y libgl1-mesa-dev libegl1-mesa-dev mesa-common-dev
+
+sudo apt-get install -y libcap-dev libbz2-dev libgcrypt11-dev libpci-dev build-essential libxcursor-dev libxcomposite-dev \
+						libxdamage-dev libxrandr-dev libdrm-dev libfontconfig1-dev libxtst-dev libasound2-dev gperf \
+                        libcups2-dev libpulse-dev libudev-dev libssl-dev flex bison ruby libxss-dev libatkmm-1.6-dev
+sudo apt-get install -y flex bison gperf libxslt-dev ruby
+sudo apt-get install -y libssl-dev libxcursor-dev libxcomposite-dev libxdamage-dev libxrandr-dev libfontconfig1-dev
+sudo apt-get install -y libasound2-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev
+
+sudo add-apt-repository -y ppa:wsnipex/vaapi
+sudo apt-get update
+sudo apt-get install -y libva-dev
+
+#webengine:
+sudo apt-get install -y libcap-dev libxrandr-dev libxcomposite-dev libxcursor-dev libxtst-dev libudev-dev libpci-dev libfontconfig1-dev libxss-dev
+
