924e8e8bf6f325bbea70ed62096ee73bced536bc
AN <an-noreply@grassfish.com>
2016-07-06 18:07:14 +0200

    hwsupport/fetch.sh: bestehenden tarball löschen

--- hwsupport/fetch.sh
+++ hwsupport/fetch.sh
@@ -4,6 +4,10 @@ set -e
 BASEDIR=$(dirname "$0")
 source $BASEDIR/../config.sh
 
+if [ -e $BASEDIR/../gf-hwsupport.tar.gz ]; then
+    rm $BASEDIR/../gf-hwsupport.tar.gz
+fi
+
 # libva
 if [ ! -e "libva-$LIBVA_VERSION.tar.bz2" ]; then
     wget https://www.freedesktop.org/software/vaapi/releases/libva/libva-$LIBVA_VERSION.tar.bz2
