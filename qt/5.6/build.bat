143b485033136d48053516d0a58ab26df7db73e0
AN <an-noreply@grassfish.com>
2016-07-06 11:07:50 +0200

    qt/build.bat: openssl-linked

--- qt/5.6/build.bat
+++ qt/5.6/build.bat
@@ -7,9 +7,9 @@ rem build qt
 if "%GF_QT_IS_32_BIT_BUILD%"=="" (set OSSL_PREFIX=C:\openssl64) else (set OSSL_PREFIX=C:\openssl32)
 set LIB=%OSSL_PREFIX%\lib;%OSSL_PREFIX%\bin;%OSSL_PREFIX%;%LIB%
 set INCLUDE=%OSSL_PREFIX%;%OSSL_PREFIX%\include;%OSSL_PREFIX%\inc32;%INCLUDE%
-
+set OPENSSL_LIBS=-L%OSSL_PREFIX%\lib -llibeay32
 jom clean
-cmd /C configure -prefix %QT_DIR% -opensource -nomake examples -nomake tests -confirm-license -qt-zlib -qt-libpng -qt-libjpeg -opengl dynamic
+cmd /C configure -prefix %QT_DIR% -opensource -nomake examples -nomake tests -confirm-license -qt-zlib -qt-libpng -qt-libjpeg -openssl-linked -opengl dynamic
 jom
 
 if exist %QT_DIR% rd /q /s %QT_DIR%
