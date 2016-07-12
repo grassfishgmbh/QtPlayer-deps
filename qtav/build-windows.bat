4dd16b45751386b97d0db4a8959672fada5ef510
AN <an-noreply@grassfish.com>
2016-07-12 16:42:57 +0200

    Zurückgezogener Änderungssatz: c556aa45b73d

--- qtav/build-windows.bat
+++ qtav/build-windows.bat
@@ -1,10 +1,10 @@
 rem bootstrap 64 bit build first
 
-call ..\config.bat
+
 
 set WORKSPACE_ROOT=%cd%
 
-set PATH=C:\cygwin64\bin;%QT_DIR%\bin;%PATH%
+set PATH=C:\cygwin64\bin;C:\Qt\5.6\msvc2013_64\bin;%PATH%
 call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64
 
 set ARCHLESS_INCLUDE=%INCLUDE%
@@ -34,6 +34,7 @@ set INCLUDE=%ARCHLESS_INCLUDE%
 set LIB=%ARCHLESS_LIB%
 
 rem bootstrap 32 bit build now
+set PATH=C:\Qt\5.6\msvc2013\bin;%PATH%
 call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64_x86
 
 rem build ffmpeg
