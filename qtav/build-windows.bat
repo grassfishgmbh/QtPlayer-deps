0827d290a95c312b811f90482d0dfd8a27bf386f
AN <an-noreply@grassfish.com>
2016-07-12 16:46:19 +0200

    qtav/build-windows: Disable 32bit Build und QT_VERSION für PATH berücksichtigen

--- qtav/build-windows.bat
+++ qtav/build-windows.bat
@@ -1,10 +1,10 @@
 rem bootstrap 64 bit build first
 
-
+call ..\config.bat
 
 set WORKSPACE_ROOT=%cd%
 
-set PATH=C:\cygwin64\bin;C:\Qt\5.6\msvc2013_64\bin;%PATH%
+set PATH=C:\cygwin64\bin;C:\Qt\%QT_VERSION%\msvc2013_64\bin;%PATH%
 call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64
 
 set ARCHLESS_INCLUDE=%INCLUDE%
@@ -30,34 +30,34 @@ cd %WORKSPACE_ROOT%
 bash qtav_installation.sh x86_64
 
 rem reset environment variables
-set INCLUDE=%ARCHLESS_INCLUDE%
-set LIB=%ARCHLESS_LIB%
+rem set INCLUDE=%ARCHLESS_INCLUDE%
+rem set LIB=%ARCHLESS_LIB%
 
 rem bootstrap 32 bit build now
-set PATH=C:\Qt\5.6\msvc2013\bin;%PATH%
-call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64_x86
+rem set PATH=C:\Qt\5.6\msvc2013\bin;%PATH%
+rem call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64_x86
 
 rem build ffmpeg
-bash ffmpeg_msvc.sh i686
+rem bash ffmpeg_msvc.sh i686
 
 rem fetch QtAV
-bash qtav_msvc.sh i686
+rem bash qtav_msvc.sh i686
 
 rem build QtAV
-cd deps-buildspace\build-QtAV
+rem cd deps-buildspace\build-QtAV
 
-set INCLUDE=%cd%\..\qtav-i686\include;%INCLUDE%
-set LIB=%cd%\..\qtav-i686\lib;%cd%\..\qtav-i686\bin;%LIB%
+rem set INCLUDE=%cd%\..\qtav-i686\include;%INCLUDE%
+rem set LIB=%cd%\..\qtav-i686\lib;%cd%\..\qtav-i686\bin;%LIB%
 
-qmake ../QtAV/QtAV.pro
-jom -j8
+rem qmake ../QtAV/QtAV.pro
+rem jom -j8
 
 rem install QtAV into specific directory
 cd %WORKSPACE_ROOT%
-bash qtav_installation.sh i686
+rem bash qtav_installation.sh i686
 
-set INCLUDE=%ARCHLESS_INCLUDE%
-set LIB=%ARCHLESS_LIB%
+rem set INCLUDE=%ARCHLESS_INCLUDE%
+rem set LIB=%ARCHLESS_LIB%
 
 rem package up the binaries
 bash qtav_package_win.sh
