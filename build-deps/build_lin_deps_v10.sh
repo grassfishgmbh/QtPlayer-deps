97bf80bb544aa6f28bb1d888441470baafaa031f
AN <an-noreply@grassfish.com>
2016-07-27 15:19:13 +0200

    build-deps/build_lin_deps_v10: Bauen von NetworkManager-Qt

--- build-deps/build_lin_deps_v10.sh
+++ build-deps/build_lin_deps_v10.sh
@@ -4,8 +4,18 @@ set -e
 
 source ../config.sh
 
+MY_ROOT=`pwd`
+
+cd ../networkmanager-qt
+
+bash prepare.sh
+bash fetch.sh
+bash build.sh
+
+cd $MY_ROOT
 cd lin
 
 bash prepare.sh
 bash fetch.sh
 bash build.sh
+
