a585b0ab5fafd2d219f6b14b9f9460b3487cddce
AN <an-noreply@grassfish.com>
2016-09-07 23:26:54 +0200

    dbus/build.sh: Erzwinge /var als Local State Directory, behebt potenzielle Probleme beim Verbinden auf den System Bus

--- dbus/build.sh
+++ dbus/build.sh
@@ -3,6 +3,6 @@
 source ../config.sh
 
 cd $DBUS_SRC_DIR
-./configure --prefix=$INSTALL_PREFIX --disable-doxygen-docs --disable-xml-docs --disable-static --disable-systemd
+./configure --prefix=$INSTALL_PREFIX --disable-doxygen-docs --disable-xml-docs --disable-static --disable-systemd --localstatedir=/var
 make -j`nproc`
 sudo make install
