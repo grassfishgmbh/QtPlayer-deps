91b7fa181d701851ee56b2be73aec5a3e2ed1c6d
AN <an-noreply@grassfish.com>
2016-09-08 13:14:46 +0200

    build-deps: Liefer nur einen Fehlerstatus wenn das Archiv nicht existiert

--- build-deps/lin/package.sh
+++ build-deps/lin/package.sh
@@ -9,4 +9,10 @@ if [ -e gf-builddeps.tar.gz ]; then
     rm -rf gf-builddeps.tar.gz
 fi
 
-tar cvzf gf-builddeps.tar.gz $INSTALL_PREFIX $NMQT_INSTALL_PREFIX
+tar cvzf gf-builddeps.tar.gz $INSTALL_PREFIX $NMQT_INSTALL_PREFIX || true
+
+if [ -f gf-builddeps.tar.gz ]; then
+    exit 0
+else
+    exit 1
+fi
