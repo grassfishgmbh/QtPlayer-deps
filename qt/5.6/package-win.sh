9ef8fae9bff503410155df8ae128f71af7306894
AN <an-noreply@grassfish.com>
2016-09-30 14:11:55 +0200

    qt/5.6/package-win: Fix Kopieren der OpenSSL DLLs ins Qt-Verzeichnis

--- qt/5.6/package-win.sh
+++ qt/5.6/package-win.sh
@@ -9,13 +9,14 @@ source $BASEDIR/../../config.sh
 
 # zip archive for dev usage
 QT_DIR_CYG=`cygpath -a $QT_DIR_WIN`
-cp $OSSL_PREFIX/bin/*.dll $QT_DIR_CYG/bin
+OSSL_PREFIX_CYG=`cygpath -a $OSSL_PREFIX`
+cp $OSSL_PREFIX_CYG/bin/*.dll $QT_DIR_CYG/bin/
 cd $QT_DIR_CYG/../..
 
 echo "Moving Qt-$QT_VERSION.zip to $ZIPTARGET"
 zip -y --symlinks -r Qt-$QT_VERSION.zip $QT_VERSION
 
-mv Qt-$QT_VERSION.zip $ZIPTARGET
+mv Qt-$QT_VERSION.zip $ZIPTARGET/
 
 echo $BUILD_VERSION > $ZIPTARGET/version.txt
 md5sum $ZIPTARGET/Qt-$QT_VERSION.zip > $ZIPTARGET/qtframework.txt
