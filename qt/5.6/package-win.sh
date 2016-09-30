c8ab6c9e598afa2e244ebcf2dcf004185e370ae2
AN <an-noreply@grassfish.com>
2016-09-30 10:19:23 +0200

    qt: Versuche Windows Build-Fix

--- qt/5.6/package-win.sh
+++ qt/5.6/package-win.sh
@@ -3,15 +3,20 @@
 set -e
 
 BASEDIR=$(dirname "$0")
+ZIPTARGET=$BASEDIR/../..
 
 source $BASEDIR/../../config.sh
-
-ZIPTARGET=`pwd`/../..
 
 # zip archive for dev usage
 QT_DIR_CYG=`cygpath -a $QT_DIR_WIN`
 cp $OSSL_PREFIX/bin/*.dll $QT_DIR_CYG/bin
 cd $QT_DIR_CYG/../..
+
+echo "Moving Qt-$QT_VERSION.zip to $ZIPTARGET"
 zip -y --symlinks -r Qt-$QT_VERSION.zip $QT_VERSION
+
 mv Qt-$QT_VERSION.zip $ZIPTARGET
+
+echo $BUILD_VERSION > $ZIPTARGET/version.txt
+md5sum $ZIPTARGET/Qt-$QT_VERSION.zip > $ZIPTARGET/qtframework.txt
 
