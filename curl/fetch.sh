9da1433fd414be3e4df46ebc5fe5bb38f028da1b
AN <an-noreply@grassfish.com>
2016-10-08 09:54:14 +0200

    curl Scripts

---
+++ curl/fetch.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+source ../config.sh
+
+function download {
+    if [ ! -f "$CURL_TARGET$CURL_FILENAME" ]; then
+        wget "$CURL_TAR_URL" -O $CURL_TARGET$CURL_FILENAME
+    fi
+}
+
+function check {
+    if [ -e "$CURL_TARGET$CURL_FILENAME" ]; then
+        HASH=`sha256sum $CURL_TARGET$CURL_FILENAME | awk -F" " '{print $1}'`
+        if [ "$HASH" != "$CURL_SHA256" ]; then
+            echo "$HASH != $CURL_SHA256"
+            rm "$CURL_TARGET$CURL_FILENAME"
+        fi
+    fi
+}
+
+if [ ! -e $CURL_TARGET ]; then
+    mkdir -p $CURL_TARGET
+fi
+
+echo "$CURL_TARGET$CURL_FILENAME"
+
+if [ ! -e "$CURL_TARGET$CURL_FILENAME" ]; then
+    echo "Downloading"
+    download
+    check
+fi
+
+# Something went wrong, so just bail
+if [ ! -e "$CURL_TARGET$CURL_FILENAME" ]; then
+    echo "$CURL_TARGET$CURL_FILENAME does not exist. Bailing."
+    exit 1
+fi
+
+echo "src fetch successful"
+exit 0
