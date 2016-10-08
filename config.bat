c30353ae870a98747c5d5a099d8d27c9a34c3726
AN <an-noreply@grassfish.com>
2016-10-08 09:56:40 +0200

    config.bat: Setzen der Buildumgebung nun generisch (für zukünftige Verwendungszwecke)

--- config.bat
+++ config.bat
@@ -5,7 +5,8 @@ set QTWEBENGINE_VERSION=5.7.0
 set QT_PATCH_DIR=%CONFIG_DIR%\qt\patches
 if "%QT_SRC_DIR%"=="" (set QT_SRC_DIR=%CONFIG_DIR%\qt\5.6\qt-src)
 if "%GF_QT_IS_32_BIT_BUILD%"=="" (set QT_DIR=C:\Qt\%QT_VERSION%\msvc2013_64) else (set QT_DIR=C:\Qt\%QT_VERSION%\msvc2013)
-
+if "%GF_QT_IS_32_BIT_BUILD%"=="" (CALL "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64) else (CALL "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64_x86)
+if "%GF_QT_IS_32_BIT_BUILD%"=="" (set OSSL_PREFIX=C:\openssl64) else (set OSSL_PREFIX=C:\openssl32)
 
 set VLC_VERSION=2.2.1
 
--- openssl/build.bat
+++ openssl/build.bat
@@ -3,8 +3,6 @@ set BASEDIR=%CD%
 CALL %BASEDIR%\..\config.bat
 
 if "%GF_QT_IS_32_BIT_BUILD%"=="" (set OSSL_TARGET=VC-WIN64A & set CONFIG_BATCH=do_win64a) else (set OSSL_TARGET=VC-WIN32 no-asm & set CONFIG_BATCH=do_ms)
-if "%GF_QT_IS_32_BIT_BUILD%"=="" (CALL "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64) else (CALL "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64_x86)
-if "%GF_QT_IS_32_BIT_BUILD%"=="" (set OSSL_PREFIX=C:\openssl64) else (set OSSL_PREFIX=C:\openssl32)
 cd openssl-%OPENSSL_VERSION%
 
 perl Configure enable-shared %OSSL_TARGET% --prefix=%OSSL_PREFIX%
