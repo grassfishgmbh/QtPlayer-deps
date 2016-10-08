ad06df00ad7d84b1c9ba38be379851872333d5a2
AN <an-noreply@grassfish.com>
2016-10-08 09:59:18 +0200

    Importiere quazip

---
+++ quazip/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+BASEDIR=$(dirname "$0")
+
+source $BASEDIR/../config.sh
+
+if [ -e build ]; then
+    rm -rf build
+fi
+mkdir build
+cd build
+
+qmake ../quazip.pro PREFIX=$INSTALL_PREFIX CONFIG+=debug_and_release
+
+make -j`nproc`
+sudo cp -r libquazip.so* $INSTALL_PREFIX/lib/
+cd ..
+sudo mkdir -p $INSTALL_PREFIX/include/quazip
+sudo cp *.h $INSTALL_PREFIX/include/quazip/
---
+++ quazip/zlib.h
@@ -0,0 +1,8 @@
+#ifndef _ZLIB_SHIM_H
+#define _ZLIB_SHIM_H
+
+#ifndef _ZLIB_H
+#include <QtZlib/zlib.h>
+#endif // _ZLIB_H
+
+#endif // _ZLIB_SHIM_H
