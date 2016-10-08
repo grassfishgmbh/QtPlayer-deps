34fe9dcad773c360bb369a8f4a32c5b238361706
AN <an-noreply@grassfish.com>
2016-10-08 09:52:34 +0200

    Importiere qtpdfium

---
+++ qtpdfium/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+BASEDIR=$(dirname "$0")
+
+source $BASEDIR/../config.sh
+
+if [ -e build ]; then
+    rm -rf build
+fi
+mkdir build
+cd build
+
+qmake ../qtpdfium.pro PREFIX=$INSTALL_PREFIX CONFIG+=debug_and_release
+
+make -j`nproc`
+sudo cp -r {include,lib,mkspecs} $INSTALL_PREFIX/
