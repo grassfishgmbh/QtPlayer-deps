169c4e062e849a582431633f3a30b5da2d56e22e
Erwin Kandler <erwin.kandler@grassfish.com>
2016-10-13 10:01:59 +0200

    config.sh: add qmake bin to PATH for kdsoap

--- config.sh
+++ config.sh
@@ -7,6 +7,7 @@ QTWEBENGINE_VERSION="5.7.0"
 QT_DIR="/opt/Qt/$QT_VERSION/gcc_64"
 QT_PATCH_DIR="$CONFIG_DIR/qt/patches"
 QT_NO_CLEAN_SRC=0
+PATH=$QT_DIR/bin:$PATH
 
 if [ `uname -o` != "GNU/Linux" ]; then
     alias sudo=
