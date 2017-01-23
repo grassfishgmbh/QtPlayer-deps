4f5c495f16ba12aa3313cd2e91a8a3496bd47517
alfred.nuc-windows <devnull@localhost>
2017-01-23 15:05:06 +0100

    qtav/ffmpeg_msvc: Verwende $FFMPEG_VERSION

--- qtav/ffmpeg_msvc.sh
+++ qtav/ffmpeg_msvc.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source ../config.sh
+
 # additionally export PATH variables as suggested by the ffmpeg wiki 
 if [ "$1" == "x86_64" ]; then
     export PATH="/cygdrive/c/Program Files (x86)/Microsoft Visual Studio 12.0/VC/BIN/amd64/":$PATH
@@ -20,18 +22,18 @@ DEPS_BS_ROOT=`pwd`
 
 INSTALL_PREFIX="$DEPS_BS_ROOT"/qtav-$1
 
-if [ ! -e ffmpeg-2.8.6.tar.bz2 ]; then
-    wget http://ffmpeg.org/releases/ffmpeg-2.8.6.tar.bz2
+if [ ! -e ffmpeg-$FFMPEG_VERSION.tar.bz2 ]; then
+    wget http://ffmpeg.org/releases/ffmpeg-$FFMPEG_VERSION.tar.bz2
 fi
 
-if [ -e ffmpeg-2.8.6 ]; then
-    rm -rf ffmpeg-2.8.6
+if [ -e ffmpeg-$FFMPEG_VERSION ]; then
+    rm -rf ffmpeg-$FFMPEG_VERSION
 fi
 
-tar xvf ffmpeg-2.8.6.tar.bz2
+tar xvf ffmpeg-$FFMPEG_VERSION.tar.bz2
 
 # BUILD FFMPEG
-cd ffmpeg-2.8.6
+cd ffmpeg-$FFMPEG_VERSION
 ./configure --extra-version=Grassfish \
             --enable-pic \
             --toolchain=msvc \
