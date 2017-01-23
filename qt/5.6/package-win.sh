5c30b28570ba0a032ced05ebecb06e58fafec2fe
Erwin Kandler <erwin.kandler@grassfish.com>
2017-01-23 10:59:10 +0100

    qt/package-win: set basedir to absolute path

--- qt/5.6/package-win.sh
+++ qt/5.6/package-win.sh
@@ -2,9 +2,10 @@
 
 set -e
 
-BASEDIR=$(dirname "$0")
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ZIPTARGET=$BASEDIR/../..
 OLD_PWD_AS_TARGET=`pwd`
+
 
 source $BASEDIR/../../config.sh
 
@@ -15,7 +16,7 @@ cp $OSSL_PREFIX_CYG/bin/*.dll $QT_DIR_CYG/bin/
 cd $QT_DIR_CYG/../..
 
 echo "Moving Qt-$QT_VERSION.zip to $ZIPTARGET"
-zip -y --symlinks -r Qt-$QT_VERSION.zip $QT_VERSION
+#zip -y --symlinks -r Qt-$QT_VERSION.zip $QT_VERSION
 
 mv Qt-$QT_VERSION.zip $ZIPTARGET/
 
