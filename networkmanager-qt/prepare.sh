12808b66325d415e354057bdacbf7ebfba0f29d6
Erwin Kandler <erwin.kandler@grassfish.com>
2017-01-26 11:38:08 +0100

    networkmanager-qt/prepare:: fix removing packages

--- networkmanager-qt/prepare.sh
+++ networkmanager-qt/prepare.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo apt-get install -y libnm-dev
-sudo apt-get remove network-manager-dev libnm-util-dev libnm-glib-dev
+sudo apt-get remove -y network-manager-dev libnm-util-dev libnm-glib-dev
