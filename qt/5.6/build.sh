39610853a3014ae780e6740951d464c9c32e94a9
AN <an-noreply@grassfish.com>
2017-03-01 15:30:44 +0100

    qt/build: Konfiguriere Build mit "-gstreamer 1.0"

--- qt/5.6/build.sh
+++ qt/5.6/build.sh
@@ -20,7 +20,7 @@ cd $QT_SRC_DIR
 # build qt
 #mkdir ../install
 #-prefix $WORKSPACE/install 
-./configure -prefix /opt/Qt/$QT_VERSION/gcc_64 -opensource -nomake examples -nomake tests -confirm-license -qt-zlib -qt-libpng -qt-libjpeg -qt-xcb -qt-pcre
+./configure -prefix /opt/Qt/$QT_VERSION/gcc_64 -opensource -nomake examples -nomake tests -confirm-license -qt-zlib -qt-libpng -qt-libjpeg -qt-xcb -qt-pcre -gstreamer 1.0
 make -j`nproc`
 
 if [ -e /opt/Qt/$QT_VERSION/ ]; then
