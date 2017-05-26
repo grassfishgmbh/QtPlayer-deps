46f980b3487693d6302c708b8db47120df9df7ff
AN <an-noreply@grassfish.com>
2017-05-26 16:59:00 +0200

    lirios-wayland/build: Erzwinge qml und plugin Subdirs

--- lirios-wayland/build.sh
+++ lirios-wayland/build.sh
@@ -11,5 +11,10 @@ cd $LIRIOSWAYLAND_TARGET
 qbs setup-toolchains --type gcc /usr/bin/g++ gcc
 qbs setup-qt $QT_DIR/bin/qmake qt5
 qbs config profiles.qt5.baseProfile gcc
-sudo env "PATH=$PATH" "LD_LIBRARY_PATH=$LD_LIBRARY_PATH" qbs -d build -j $(nproc) profile:qt5 qbs.installRoot:${INSTALL_PREFIX}
-
+sudo env "PATH=$PATH" "LD_LIBRARY_PATH=$LD_LIBRARY_PATH" qbs -d build -j $(nproc) profile:qt5 \
+    qbs.installRoot:${INSTALL_PREFIX} \
+    lirideployment:libDir=lib \
+    lirideployment:qmlDir=qml \
+    lirideployment:pluginsDir=plugins \
+    qbsbuildconfig:withKmsPlugin=true \
+    qbsbuildconfig:withXWayland=true
