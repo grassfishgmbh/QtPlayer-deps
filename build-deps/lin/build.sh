61afac6eeb4d0e651bfe7bb5c54de44b92292b82
AN <an-noreply@grassfish.com>
2017-07-19 18:23:08 +0200

    build-deps/lin: Selbstgebautes DBus als obsolet (damals notwendig für Ubuntu 14.04)

--- build-deps/lin/build.sh
+++ build-deps/lin/build.sh
@@ -133,10 +133,10 @@ bash prepare.sh
 bash fetch.sh
 bash build.sh
 
-cd $MY_ROOT/../dbus
-bash fetch.sh
-bash unpack.sh
-bash build.sh
+#cd $MY_ROOT/../dbus
+#bash fetch.sh
+#bash unpack.sh
+#bash build.sh
 
 cd $MY_ROOT/../openssl
 bash fetch.sh
