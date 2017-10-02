68a2fab3bf6281ab0808771ab79ed2d3c13c8028
AN <an-noreply@grassfish.com>
2017-10-02 14:52:30 +0200

    QtWebApp: Baue im Debug-Mode

--- QtWebApp/build.sh
+++ QtWebApp/build.sh
@@ -15,7 +15,7 @@ cd build
 qmake ../QtWebApp.pro PREFIX=$INSTALL_PREFIX CONFIG+=debug_and_release
 
 make -j`nproc`
-sudo cp -r libQtWebApp.so* $INSTALL_PREFIX/lib/
+sudo cp -r libQtWebApp*.so* $INSTALL_PREFIX/lib/
 cd ..
 sudo mkdir -p $INSTALL_PREFIX/include/QtWebApp/httpserver
 sudo cp -r httpserver/*.h $INSTALL_PREFIX/include/QtWebApp/httpserver
