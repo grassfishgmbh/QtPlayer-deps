7a66890f43f1548354d45b1cff69fe03b37fd2c5
Erwin Kandler <erwin.kandler@grassfish.com>
2017-12-15 11:55:48 +0100

    build qtpdfium with submodule

---
+++ qtpdfium/fetch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $BASEDIR
+source $BASEDIR/../config.sh
+
+git submodule update --init --recursive src
+
+echo "src fetch successful"
+exit 0
